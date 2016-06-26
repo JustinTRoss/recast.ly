7f5e94ec4a0726770a136eaa351f4ff1ffa372ca
JustinTRoss <ross.justin.t@gmail.com>
2016-06-25 17:09:18 -0700

    Implement fetch call to API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,23 +1,37 @@
 var searchYouTube = (options, callback) => {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    method: 'GET',
-    data: {
-      part: 'snippet',
-      type: 'video',
-      videoEmbeddable: true,
-      q: options.query || 'marmoset',
-      key: options.key || YOUTUBE_API_KEY,
-      maxResults: options.max || 5
-    },
-    success: function(data) {
-      console.log('data received');
-      callback(data.items);
-    },
-    error: function() {
-      console.log('error!');
-    }
+
+  var generateUrl = () => {
+    return `https://www.googleapis.com/youtube/v3/search?key=${options.key || YOUTUBE_API_KEY}&part=snippet&type=video&videoEmbeddable=true&q=${options.query || 'dogs'}&maxResults=${options.max || 10}`;
+  };
+
+  fetch(generateUrl()).then(response => {
+    return response.json();
+  }).then(data => {
+    console.log('Data received');
+    callback(data.items);
+  }).catch(() => {
+    console.log("Booo");
   });
+  
+  // $.ajax({
+  //   url: 'https://www.googleapis.com/youtube/v3/search',
+  //   method: 'GET',
+  //   data: {
+  //     part: 'snippet',
+  //     type: 'video',
+  //     videoEmbeddable: true,
+  //     q: options.query || 'marmoset',
+  //     key: options.key || YOUTUBE_API_KEY,
+  //     maxResults: options.max || 5
+  //   },
+  //   success: function(data) {
+  //     console.log('data received');
+  //     callback(data.items);
+  //   },
+  //   error: function() {
+  //     console.log('error!');
+  //   }
+  // });
 };
 
 window.searchYouTube = searchYouTube;
